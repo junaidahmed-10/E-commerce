87c7c726ef6acb720f09163c12222541b0c550fb
junaid <junaidsheikh45@gmail.com>
2024-10-08 01:20:19 +0530

    new components added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,27 +9,57 @@ import AdminOrders from "./pages/admin/orders"
 import AdminFeatures from "./pages/admin/features"
 import ShoppingLayout from "./components/customer/layout"
 import NotFound from "./pages/NotFound/notFound"
+import Account from "./pages/customer/account"
+import Checkout from "./pages/customer/checkout"
+import Listing from "./pages/customer/listing"
+import Home from "./pages/customer/home"
+import CheckAuth from "./components/common/checkAuth"
 
 
 
 
 function App() {
 
+  const isAuthenticated = true;
+  const user = {
+    name : "junaid",
+    role : 'user'
+  };
+
   return (
     <div className="flex flex-col overflow-hidden bg-white">
 
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="/auth" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AuthLayout />
+          </CheckAuth>
+        }>
           <Route path="signIn" element={<AuthSignIn />} />
           <Route path="signUp" element={<AuthSignUp />} />
         </Route>
-        <Route path="/admin" element={<AdminLayout />}>
+
+        <Route path="/admin" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <AdminLayout />
+          </CheckAuth>
+        }>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
         </Route>
-        <Route path="/shop" element={<ShoppingLayout />}></Route>
+        
+        <Route path="/shop" element={
+          <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+            <ShoppingLayout />
+          </CheckAuth>
+        }>
+          <Route path="home" element={<Home />} />
+          <Route path="accounts" element={<Account />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="listing" element={<Listing />} />
+        </Route>
 
         <Route path="*" element={<NotFound />} />
       </Routes>
---
+++ client/src/pages/unAuthPage/unAuthPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UnAuthPage() {
+    return (
+        <div><h1>
+            you dont have access to this page
+        </h1></div>
+    )
+}
+
+export default UnAuthPage
